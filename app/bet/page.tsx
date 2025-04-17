e5ee01b9c60f6e95df6b19dbac14eecee61dcd40
DigitalDominance <digitaldominanceseo@gmail.com>
2025-04-16 20:28:31 -0400

    Update page.tsx

--- app/bet/page.tsx
+++ app/bet/page.tsx
@@ -371,8 +371,8 @@ export default function BettingPage() {
         {sports.map((sport) => {
           const isSelected = selectedSport === sport.key;
           const btnClass = isSelected
-            ? "bg-[#49EACB] text-black px-10 py-5 text-3xl"
-            : "bg-gray-800 text-white px-10 py-5 text-3xl";
+            ? "bg-[#49EACB] text-black px-10 py-5 text-5xl"
+            : "bg-gray-800 text-white px-10 py-5 text-5xl";
           return (
             <Button
               key={sport.key}
@@ -385,8 +385,8 @@ export default function BettingPage() {
                   <Image
                     src={iconMap[sport.key]}
                     alt={`${sport.title} icon`}
-                    width={32}
-                    height={32}
+                    width={50}
+                    height={50}
                     className="ml-2"
                   />
                 )}
